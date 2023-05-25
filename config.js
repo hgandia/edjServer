b075bc96c05fae8a4531bbeb2b1af3bf5f39074a
Herbert Gandia <106564505+hgandia@users.noreply.github.com>
2023-05-25 15:18:59 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    secretKey: '53E184ST',
-    mongoUrl: 'mongodb://localhost:27017/edj',
-    mongoConnectionString: 'mongodb+srv://la1raestrelladejacob:lamaslindadelbronx53184@cluster0.a1xgqhl.mongodb.net/?retryWrites=true&w=majority',
-    bucketName: 'YOUR GOOGLE CLOUD STORAGE BUCKET NAME HERE'
-}
